30dba2181aeb3edafafe5fb6ead1605fdac6e388
Jatin Bhatnagar <jatinbhatnagar05@gmail.com>
2020-10-23 03:20:22 +0530

    Add Auth module sign up, do salt next

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), NotesModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), NotesModule, AuthModule],
 })
 export class AppModule {}
---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must be 8 characters long and contain a letter in [A-Z],a letter in [a-z] and, a digit[0-9] or special char(*,$ etc)',
+  })
+  password: string;
+}
